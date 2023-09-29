ee9aa74e8e98ecbd53e0d6cd38965bf71f8f1136
Kiru <37779457+kiruthesh08@users.noreply.github.com>
2023-09-29 22:33:15 +0100

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,4 +1,15 @@
-import unittest
-from hello_world import helloworld 
+import pytest
+
+from hello_world import helloworld
+
+# Define a test function using 'test_' 
 def test_hello_world():
-    assert helloworld() == "hello-world"
+    # Call function and store result
+    result = helloworld()
+
+    # Define expected result
+    expected_result = "hello_world"
+
+    #assertion to check if the result matches the expected result
+    assert result == expected_result
+    
